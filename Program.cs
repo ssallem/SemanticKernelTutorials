2de482d9b082b6f0580e63b3604a70b07f4eb7b8
ssallem <ssallem@nate.com>
2025-05-13 16:26:52 +0900

    1. Make Chat completion

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ var builder = Host.CreateApplicationBuilder(args);
 IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
 #pragma warning disable SKEXP0070 // 형식은 평가 목적으로 제공되며, 이후 업데이트에서 변경되거나 제거될 수 있습니다. 계속하려면 이 진단을 표시하지 않습니다.
 kernelBuilder.AddOllamaChatCompletion(
-    modelId: "deepseek-r1:14b",
+    modelId: "deepseek-r1:1.5b",
     endpoint: new Uri("http://localhost:11434"),
-    serviceId: "TEST_ID"
+    serviceId: "ChatService"
     );
 #pragma warning restore SKEXP0070 // 형식은 평가 목적으로 제공되며, 이후 업데이트에서 변경되거나 제거될 수 있습니다. 계속하려면 이 진단을 표시하지 않습니다.
 
