adb312e655b380e8996fa5e4e0c54a3e75ae827b
spelix01 <spelix01@WIN-BOKCHQIND88>
2025-05-13 16:19:03 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+#pragma warning disable SKEXP0070 // 형식은 평가 목적으로 제공되며, 이후 업데이트에서 변경되거나 제거될 수 있습니다. 계속하려면 이 진단을 표시하지 않습니다.
+kernelBuilder.AddOllamaChatCompletion(
+    modelId: "deepseek-r1:14b",
+    endpoint: new Uri("http://localhost:11434"),
+    serviceId: "TEST_ID"
+    );
+#pragma warning restore SKEXP0070 // 형식은 평가 목적으로 제공되며, 이후 업데이트에서 변경되거나 제거될 수 있습니다. 계속하려면 이 진단을 표시하지 않습니다.
+
+builder.Services.AddTransient((serviceProvider) =>
+{
+    return new Kernel(serviceProvider);
+});
+
+Kernel kernel = kernelBuilder.Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+ChatHistory history = [];
+history.AddUserMessage("Please tell me how to check which port is being used in Windows.");
+
+
+var response = chatCompletionService.GetStreamingChatMessageContentsAsync(
+    chatHistory: history,
+    kernel: kernel
+);
+
+await foreach (var chunk in response)
+{
+    Console.Write(chunk);
+}
+Console.WriteLine();
+Console.ReadLine();
